4815047980a017392ee2281e3e30919b5da56ad7
Gustavo <gus.aquino42@gmail.com>
2022-07-08 02:57:14 -0300

    commit de correção

--- Vendas.hpp
+++ Vendas.hpp
@@ -11,9 +11,9 @@ private:
   std::map<int,std::list<listaDeCompra>> _vendasCaixa; static int codigo_vendas;
 public:
   void nova_venda(Estoque& estoquePrincipal, std::list<listaDeCompra> &compra);//reduz o estoque, chama a cobrança, calcula o troco e imprime a nota fiscal
-  double cobranca(double valorDaCompra, bool dinheiroEspecie);
-  void imprimir_nota_fiscal();
-  int fechamento_de_caixa();
+  double cobranca(double valorDaCompra, bool dinheiroEspecie); // Calcula o valor da compra, escolhe a opção dinheiro ou cartão e faz o calculo de troco se necesário
+  void imprimir_nota_fiscal(); //Imprime a lista de produtos da mesma forma que uma nota de supermercado comum
+  int fechamento_de_caixa(); // calcula o total de vendas do caixa e se o valor bate com o que têm em caixa
 };
 
 #endif
